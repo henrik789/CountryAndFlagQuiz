94812f11977e5e9b6899b9ed9dfa9cc977391d00
Henrik789 <henrik789@github.com>
2020-03-28 10:02:44 +0100

    Initial Commit

---
+++ CountryAndFlagQuiz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CountryAndFlagQuiz
+//
+//  Created by Henrik on 2020-03-28.
+//  Copyright © 2020 Henrik. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
